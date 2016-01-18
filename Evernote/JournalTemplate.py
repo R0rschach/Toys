bca4d8d2528e890dea951e52b1e9d77fd619527a
Lei Du <dulei66@gmail.com>
2016-01-18 12:06:36 -0800

    feat: evernote journal and weekly review auto propagation

--- Evernote/JournalTemplate.py
+++ Evernote/JournalTemplate.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 import sys
 import mybook
 
-def create_daily_journal(date, content = ''):
+
+def create_daily_journal(date, content=''):
     """
     Create a daily journal of given date. 
     Creation time will be equal to the date. 
@@ -13,10 +14,11 @@ def create_daily_journal(date, content = ''):
     """
     title = date.strftime('%Y.%m.%d')
     if not content:
-        content = open('template.daily_journal.html','r').read()
-    return (title, content, date) 
+        content = open('template.daily_journal.html', 'r').read()
+    return (title, content, date)
 
-def create_weekly_summary(date, content = ''):
+
+def create_weekly_summary(date, content=''):
     """
     Create a weekly summary journal of given date. 
     Creation time will be equal to the date. 
@@ -27,24 +29,27 @@ def create_weekly_summary(date, content = ''):
     """
     title = date.strftime('%Y.%m.%d Weekly Summary')
     if not content:
-        content = open('template.weekly_summary.html','r').read()
+        content = open('template.weekly_summary.html', 'r').read()
     return (title, content, date)
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('Generate journals template for next 7 days')
-        mybook = mybook.MyBook()
-
         dt = datetime.today()
-        dt = datetime(dt.year, dt.month, dt.day)
-        end = dt + timedelta(days=7)
-        while dt <= end:
-            print('Create Daily Journal: %s' % dt.strftime('%Y-%m-%d'))
-            title, content, created = create_daily_journal(dt)
+    elif len(sys.argv) == 2:
+        dt = datetime.strptime(sys.argv[1], '%Y-%m-%d')
+
+    mybook = mybook.MyBook()
+
+    dt = datetime(dt.year, dt.month, dt.day)
+    end = dt + timedelta(days=7)
+    while dt <= end:
+        print('Create Daily Journal: %s' % dt.strftime('%Y-%m-%d'))
+        title, content, created = create_daily_journal(dt)
+        mybook.create_note(title, content, created=created)
+        if dt.weekday() == 6:
+            print('Create Weely Summary: %s' % dt.strftime('%Y-%m-%d'))
+            title, content, created = create_weekly_summary(dt + timedelta(seconds=1))
             mybook.create_note(title, content, created=created)
-            if dt.weekday() == 6:
-                print('Create Weely Summary: %s' % dt.strftime('%Y-%m-%d'))
-                title, content, created = create_weekly_summary(dt+timedelta(seconds=1))
-                mybook.create_note(title, content, created=created)
-            dt += timedelta(days=1)
+        dt += timedelta(days=1)
--- Evernote/mybook.py
+++ Evernote/mybook.py
@@ -1,21 +1,19 @@
-import evernote.edam.userstore.constants as UserStoreConstants
-import evernote.edam.type.ttypes as EnTypes
-
-from evernote.api.client import EvernoteClient
-import sys
-import time
 from datetime import datetime, timedelta
 
+import evernote.edam.type.ttypes as EnTypes
+from evernote.api.client import EvernoteClient
+
+
 def convert_time(local_dt):
-    delta = (datetime.utcfromtimestamp(local_dt.timestamp()) - datetime(1970, 1, 1))
-    stamp = delta / timedelta(milliseconds = 1)
-    return int(stamp)
+    delta = (local_dt - datetime.utcfromtimestamp(0)).total_seconds()
+    return int(delta * 1000)
+
 
 class MyBook():
-    def __init__(self, dev_tok_file = 'dev_token.txt'):
-        devtok = open(dev_tok_file,'r').read().strip()
-
-        self.client = EvernoteClient(token = devtok, sandbox = False)
+    def __init__(self, dev_tok_file='dev_token.txt'):
+        devtok = open(dev_tok_file, 'r').read().strip()
+        self.token = devtok
+        self.client = EvernoteClient(token=devtok, sandbox=False)
         self.user_store = self.client.get_user_store()
         self.note_store = self.client.get_note_store()
         pass
@@ -28,10 +26,3 @@ class MyBook():
             note.created = convert_time(created)
         created_note_guid = self.note_store.createNote(note)
         print('Created Note: %s' % created_note_guid)
-
-
-
-
-
-
-
